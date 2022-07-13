9acf2193c400e7a428cc3a7c8b3b0f202edb2617
Anthony Douglas Long <tdlong@login-i16.local>
2022-07-13 14:59:05 -0700

    Pollack edits

--- scripts/makePollockPlot.R
+++ scripts/makePollockPlot.R
@@ -12,19 +12,21 @@ for(i in startIndex:stopIndex){
 	filename = paste0("runs/run_",i,"/founder_history.csv")
 	founder_hist = read.csv(filename, header=T)
 	founder_color = rainbow(18)
-	# week 5
-	m = founder_hist[founder_hist$tick == 193,]
-#	m = founder_hist[founder_hist$tick == 445,]
+	m = founder_hist[founder_hist$tick == 445,]
 	start = as.numeric(unlist(m["start"]))
 	m = as.matrix(m[,1:18 + 3])
+	mstart = founder_hist[founder_hist$tick == 13,]
+	mstart = as.matrix(mstart[,1:18 + 3])	
+	m2 = m-mstart
+
 	if(i==startIndex){
 		par(mar=c(3.5, 3.5, 1, 1), family="serif", mgp=c(2.1, 0.6, 0))
-		plot(x=range(start), y=c(0, max(m)), type="n", xlab="position", ylab="frequency")
+		plot(x=range(start), y=c(0, 0.4), type="n", xlab="position", ylab="freq change")
 		}
 
-	maxPerWindow = as.integer(apply(m, MARGIN=1, FUN=which.max))
-	topFreq = sapply(1:NROW(m), FUN=function(window) { m[window, maxPerWindow[window]] })
-	topColor = sapply(1:NROW(m), FUN=function(window) { founder_color [maxPerWindow[window]] })
+	maxPerWindow = as.integer(apply(m2, MARGIN=1, FUN=which.max))
+	topFreq = sapply(1:NROW(m2), FUN=function(window) { m2[window, maxPerWindow[window]] })
+	topColor = sapply(1:NROW(m2), FUN=function(window) { founder_color [maxPerWindow[window]] })
 
 	points(start, topFreq, col=topColor, pch=19, cex=0.5)
 	lines(start, topFreq, col="lightgrey", lwd=0.25)
